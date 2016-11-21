b6412ee1f304f8b4e58d45cb85237dd544506c65
Yang Jiang <jiangyang5157@gmail.com>
2016-11-21 21:47:31 +1300

    na

---
+++ base/bubble_sort.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"github.com/jiangyang5157/go/utils"
+)
+
+func main() {
+	arr := utils.RandomArray(10)
+	fmt.Println("Initial array is:", arr)
+	for i := 0; i < len(arr); i++ {
+		for j := 0; j < len(arr) - 1; j++ {
+			if arr[j] > arr[j + 1] {
+				tmp := arr[j]
+				arr[j] = arr[j + 1]
+				arr[j + 1] = tmp
+			}
+		}
+	}
+	fmt.Println("Bubble sorted array is: ", arr)
+}
---
+++ utils/number.go
@@ -0,0 +1,11 @@
+package utils
+
+import "math/rand"
+
+func RandomArray(n int) []int {
+	ret := make([]int, n)
+	for i := 0; i < n; i++ {
+		ret[i] = rand.Intn(n)
+	}
+	return ret
+}
