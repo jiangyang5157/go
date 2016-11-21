a540a51791e7348f4e170c76ef3dc54006e79328
Yang Jiang <jiangyang5157@gmail.com>
2016-11-22 00:35:39 +1300

    insert sort

---
+++ base/insertion_sort.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"github.com/jiangyang5157/go/utils"
+)
+
+// O(n^2)
+func insertSort(arr []int) []int {
+	arrLen := len(arr);
+	for i := 1; i < arrLen; i++ {
+		for j := i; j > 0 && arr[j] < arr[j - 1]; j-- {
+			arr[j], arr[j - 1] = arr[j - 1], arr[j]
+		}
+	}
+	return arr
+}
+
+func main() {
+	arr := utils.RandomArray(10)
+	fmt.Println("Initial array is:", arr)
+	fmt.Println("Sorted array is: ", insertSort(arr))
+}
