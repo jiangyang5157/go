cd141a5de2aab151aa28846a3491bac31a2b0b60
Yang Jiang <jiangyang5157@gmail.com>
2016-11-22 00:53:26 +1300

    odd even sort

---
+++ base/odd_even_sort.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"github.com/jiangyang5157/go/utils"
+)
+
+// O(n^2)
+func oddEvenSort(arr []int) []int {
+	arrLen := len(arr);
+	for isSorted := false; isSorted == false; {
+		isSorted = true
+		for i := 0; i < arrLen - 1; i += 2 {
+			if arr[i] > arr[i + 1] {
+				arr[i], arr[i + 1] = arr[i + 1], arr[i];
+				isSorted = false
+			}
+		}
+		for i := 1; i < arrLen - 1; i += 2 {
+			if arr[i] > arr[i + 1] {
+				arr[i], arr[i + 1] = arr[i + 1], arr[i];
+				isSorted = false
+			}
+		}
+	}
+	return arr
+}
+
+func main() {
+	arr := utils.RandomArray(10)
+	fmt.Println("Initial array is:", arr)
+	fmt.Println("Sorted array is: ", oddEvenSort(arr))
+}
