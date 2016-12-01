75fb3807be2cf1b5859e54807d387768fcec8672
Yang Jiang <jiangyang5157@gmail.com>
2016-12-01 23:52:06 +1300

    dlx tests

--- dlx/dlx_test.go
+++ dlx/dlx_test.go
@@ -5,19 +5,19 @@ import (
 	"fmt"
 )
 
-func Test_NewDlx(t *testing.T) {
-	raw := "0123456789" +
-		"111111111" +
-		"222222222" +
-		"333333333" +
-		"444444444" +
-		"555555555" +
-		"666666666" +
-		"777777777" +
-		"888888888"
-	printRaw("#### TEST", raw);
-	fmt.Println("\n####")
+func Test_stuff(t *testing.T) {
+	testRaw := ".......12........3..23..4....18....5.6..7.8.......9.....85.....9...4.5..47...6..."
+	printRaw("#### test printRaw", testRaw)
 
+	fmt.Println("#### test values of char")
+	//0,  1,  2,  3,  4,  5,  6,  7,  8,  9,  .
+	//48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 46
+	fmt.Printf("%v,  %v,  %v,  %v,  %v,  %v,  %v,  %v,  %v,  %v,  %v\n", 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, ".")
+	fmt.Printf("%v, %v, %v, %v, %v, %v, %v, %v, %v, %v, %v\n", int('0'), int('1'), int('2'), int('3'), int('4'), int('5'), int('6'), int('7'), int('8'), int('9'), int('.'))
+}
+
+func Test_NewDlx(t *testing.T) {
+	fmt.Println("#### test newDlx")
 	d := newDlx(5)
 	columns := d.columns;
 	for i := range columns {
@@ -26,11 +26,10 @@ func Test_NewDlx(t *testing.T) {
 	fmt.Println("\n####")
 	h := &d.columns[0]
 	c := h.r.c
-	for ;c.x != h.x; c = c.r.c {
+	for ; c.x != h.x; c = c.r.c {
 		fmt.Print(c.i)
 	}
 	fmt.Println("\n####")
-	fmt.Printf("%v, %v, %v, %v, %v, %v, %v, %v, %v, %v, %v\n", int('0'), int('1'), int('2'), int('3'), int('4'), int('5'), int('6'), int('7'), int('8'), int('9'), int('.'))
 }
 
 func printRaw(title, raw string) {
