969f7fa06f2395b8016ff15d640e92916b7aa5c5
Yang Jiang <jiangyang5157@gmail.com>
2016-12-03 15:38:24 +1300

    NA

--- dlx/dlx_test.go
+++ dlx/dlx_test.go
@@ -5,13 +5,6 @@ import (
 	"fmt"
 )
 
-func Test_Ascii(t *testing.T) {
-	//0,  1,  2,  3,  4,  5,  6,  7,  8,  9,  .
-	//48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 46
-	fmt.Printf("%v,  %v,  %v,  %v,  %v,  %v,  %v,  %v,  %v,  %v,  %v\n", 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, ".")
-	fmt.Printf("%v, %v, %v, %v, %v, %v, %v, %v, %v, %v, %v\n", int('0'), int('1'), int('2'), int('3'), int('4'), int('5'), int('6'), int('7'), int('8'), int('9'), int('.'))
-}
-
 func Test_newDlx(t *testing.T) {
 	d := newDlx(5)
 	columns := d.columns;
--- dlx/sudoku_test.go
+++ dlx/sudoku_test.go
@@ -6,6 +6,13 @@ import (
 	"strings"
 )
 
+func Test_Ascii(t *testing.T) {
+	//0,  1,  2,  3,  4,  5,  6,  7,  8,  9,  .
+	//48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 46
+	fmt.Printf("%v,  %v,  %v,  %v,  %v,  %v,  %v,  %v,  %v,  %v,  %v\n", 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, ".")
+	fmt.Printf("%v, %v, %v, %v, %v, %v, %v, %v, %v, %v, %v\n", int('0'), int('1'), int('2'), int('3'), int('4'), int('5'), int('6'), int('7'), int('8'), int('9'), int('.'))
+}
+
 func printSudoku(squareLength int, raw string) {
 	fmt.Println("================================================================")
 	switch squareLength {
