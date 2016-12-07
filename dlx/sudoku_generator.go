469dc3519220bb0401ef10f7d6244a3a1139f128
Yang Jiang <jiangyang5157@gmail.com>
2016-12-07 17:27:12 +1300

    GeneratePuzzle

--- dlx/sudoku_generator.go
+++ dlx/sudoku_generator.go
@@ -13,14 +13,52 @@ func GeneratePuzzle(squareLength int, minSubGivens int, maxSubGivens int) string
 	p := newPuzzle(squareLength)
 	var rtp []int = p.randomTerminalPattern()
 
-	//mtg := minTotalGivens(p.edgeLength)
 	//squareLength := p.squareLength
-	//edgeLength := p.edgeLength
-	//cellSize := p.cellSize
-	//var remainRowGivens []int = make([]int, edgeLength)
-	//var remainColumnGivens []int = make([]int, edgeLength)
+	edgeLength := p.edgeLength
+	cellSize := p.cellSize
+	mtg := minTotalGivens(p.edgeLength)
+	remainTotalGivens := cellSize
+	var remainRowGivens []int = make([]int, edgeLength)
+	for i := range remainRowGivens {
+		remainRowGivens[i] = edgeLength
+	}
+	var remainColumnGivens []int = make([]int, edgeLength)
+	for i := range remainColumnGivens {
+		remainColumnGivens[i] = edgeLength
+	}
 
+	// 0,1,...,edgeLength - 1
+	var tmp []int = make([]int, edgeLength)
+	for i := range tmp {
+		tmp[i] = i
+	}
 
+	for r := 0; r < edgeLength; r++ {
+		var d []int = disorderArray(tmp)
+		for di := 0; di < edgeLength; di++ {
+			c := d[di]
+			switch {
+			case remainTotalGivens <= mtg:
+				continue
+			case remainColumnGivens[c] <= minSubGivens:
+				continue
+			case remainRowGivens[r] <= minSubGivens:
+				continue
+			default:
+				i := p.cellIndex(r, c)
+				digit := rtp[i]
+				rtp[i] = 0
+				p.init(rtp)
+				if (p.hasUniqueSolution()) {
+					remainTotalGivens--
+					remainColumnGivens[c]--
+					remainRowGivens[r]--
+				} else {
+					rtp[i] = digit
+				}
+			}
+		}
+	}
 
 	return digits2raw(rtp)
 }
@@ -31,6 +69,8 @@ func (p *puzzle) randomTerminalPattern() []int {
 	cellSize := p.cellSize
 	var ret []int = make([]int, cellSize)
 	var digits []int = make([]int, cellSize)
+
+	// 1,2,...,edgeLength
 	var tmp []int = make([]int, edgeLength)
 	for i := range tmp {
 		tmp[i] = i + 1
--- dlx/sudoku_generator_test.go
+++ dlx/sudoku_generator_test.go
@@ -2,14 +2,25 @@ package dlx
 
 import (
 	"testing"
+	"strings"
+	"fmt"
 )
 
 func Test_GeneratePuzzle(t *testing.T) {
 	squareLength := 3
-	minSubGivens := 0
-	maxSubGivens := squareLength * squareLength
-	var puzzle string = GeneratePuzzle(squareLength, minSubGivens, maxSubGivens)
-	printSudokuByRaw(squareLength, puzzle)
+	minSubGivens := 2
+	maxSubGivens := 6
+	var raw string = GeneratePuzzle(squareLength, minSubGivens, maxSubGivens)
+	printSudokuByRaw(squareLength, raw)
+
+	p := newPuzzle(squareLength)
+	p.init(raw2digits(raw))
+	var solutions []string = strings.Split(p.solvePuzzle(1), string(SOLUTION_PREFIX))
+	fmt.Printf("%v\n", solutions[0])
+	fmt.Printf("Looking for %d solutions, found %d solutions.\n", 2, len(solutions) - 1)
+	for i := 1; i < len(solutions); i++ {
+		printSudokuByRaw(squareLength, solutions[i])
+	}
 }
 
 func Test_randomTerminalPattern(t *testing.T) {
