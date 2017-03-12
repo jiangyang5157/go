2f204074433e3ab2b490ed4af94e7b7a39cf2491
Yang Jiang <jiangyang5157@gmail.com>
2017-03-12 22:32:34 +1300

    hamingdistance

---
+++ bit/hammingdistance_test.go
@@ -0,0 +1,11 @@
+package hammingdistance
+
+import "testing"
+
+func Test_HamDist(t *testing.T) {
+	hammingDistance(0, 255)
+}
+
+// func Test_totalHammingDistance(t *testing.T) {
+// 	totalHammingDistance([...]int{0, 255})
+// }
