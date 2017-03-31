b7f09eb895ab6424844c53464d384ed2e305ddf7
yangjiang <jiangyang5157@gmail.com>
2017-03-31 21:04:17 +1300

    NA

--- bit/hammingdistance.go
+++ bit/hammingdistance.go
@@ -1,12 +1,30 @@
 package hammingdistance
 
 func hammingDistance(a, b uint8) int {
-	var ret int = 0
-	var i uint
-	for i = 0; i < 8; i++ {
-		if a&(1<<i) != b&(1<<i) {
-			ret++
-		}
+	var ret = 0
+	if a&1<<0 != b&1<<0 {
+		ret++
+	}
+	if a&1<<1 != b&1<<1 {
+		ret++
+	}
+	if a&1<<2 != b&1<<2 {
+		ret++
+	}
+	if a&1<<3 != b&1<<3 {
+		ret++
+	}
+	if a&1<<4 != b&1<<4 {
+		ret++
+	}
+	if a&1<<5 != b&1<<5 {
+		ret++
+	}
+	if a&1<<6 != b&1<<6 {
+		ret++
+	}
+	if a&1<<7 != b&1<<7 {
+		ret++
 	}
 	return ret
 }
--- bit/hammingdistance_test.go
+++ bit/hammingdistance_test.go
@@ -5,8 +5,8 @@ import (
 	"testing"
 )
 
-func Test_HamDist(t *testing.T) {
-	var a, b uint8 = 0, 255
+func Test_HammingDistance(t *testing.T) {
+	a, b := uint8(0), uint8(255)
 	result := hammingDistance(a, b)
-	fmt.Printf("HammingDistance: %08b and %08b = %d\n", byte(a), byte(b), result)
+	fmt.Printf("HammingDistance: %08b and %08b = %d\n", a, b, result)
 }
