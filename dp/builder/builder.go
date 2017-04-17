8b78cee76626c2e0c5aed0097d02402cf305092d
yangjiang <jiangyang5157@gmail.com>
2017-04-17 21:42:59 +1200

    remove dp

--- dp/builder/builder.go
+++
@@ -1,7 +0,0 @@
-package builder
-
-type Builder interface {
-	Build() *Character
-	SetName(name string) Builder
-	SetArms(arms string) Builder
-}
--- dp/builder/director.go
+++
@@ -1,9 +0,0 @@
-package builder
-
-type Director struct {
-	b Builder
-}
-
-func (p Director) Create(name string, arms string) *Character {
-	return p.b.SetName(name).SetArms(arms).Build()
-}
